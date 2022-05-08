92d9e4e6bc7f9cccaecb47c6268488595ed10394
wilson6508 <wilson650837@gmail.com>
2022-05-08 23:53:40 +0800

    1

---
+++ 1.前端/1.框架/VUE/尚硅谷/1.Vue核心/course/19_單文件組件/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+// < + v + enter 快速生成模板
+import School from "./School.vue";
+export default {
+  name: "App",
+  components: {
+    School,
+  },
+};
+</script>
---
+++ 1.前端/1.框架/VUE/尚硅谷/1.Vue核心/course/19_單文件組件/School.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="demo">
+    <h2>學校名稱: {{ name }}</h2>
+    <h2>學校地址: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京",
+    };
+  },
+  methods: {},
+};
+// const school = Vue.extend({
+//   data() {
+//     return {
+//       name: "尚硅谷",
+//       address: "北京",
+//     };
+//   },
+// });
+// export { school }          // 統一暴露
+// export default school      // 默認暴露
+// export 加在const school前   // 分別暴露
+</script>
+
+<style scoped>
+.demo {
+  background-color: orange;
+}
+</style>
