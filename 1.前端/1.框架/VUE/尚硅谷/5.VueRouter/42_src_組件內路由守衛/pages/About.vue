9b0b5d24b3596da8a275693b5a3d02bc8d012184
wilson6508 <wilson650837@gmail.com>
2022-05-24 00:37:40 +0800

    1

---
+++ 1.前端/1.框架/VUE/尚硅谷/5.VueRouter/42_src_組件內路由守衛/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>我是About</h2>
+</template>
+
+<script>
+export default {
+  name: "About",
+  mounted() {
+    window.aboutRoute = this.$route;
+    window.aboutRouter = this.$router;
+  },
+  // 通過路由規則, 進入該組件時被調用
+  beforeRouteEnter(to, from, next) {
+    console.log(5566);
+    next();
+  },
+  // 通過路由規則, 離開該組件時被調用
+  beforeRouteLeave(to, from, next) {
+    next();
+  },
+};
+</script>
---
+++ 1.前端/1.框架/VUE/尚硅谷/5.VueRouter/42_src_組件內路由守衛/pages/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>消息編號:{{ id }}</li>
+    <li>消息標題:{{ title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  props: ["id", "title"], // 接收第2,3種
+  //   mounted() {
+  //     console.log(this.$route);
+  //     console.log(this.$route.query);
+  //     console.log(this.$route.params);
+  //   },
+};
+</script>
